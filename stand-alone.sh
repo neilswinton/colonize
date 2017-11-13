16b6e215a8c2d2829112d4a67fa8c5adc656654a
Neil Swinton <neil@pineknollconsulting.com>
2017-11-13 17:25:02 -0500

    Merge stand-alone.sh from github master, remove swizzled repos crap.

--- stand-alone.sh
+++ stand-alone.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+program="$0"
+
 function fatal()
 {
     if [ -n "$*" ] ; then
         echo >&2 "$*"
     fi
 
-    echo >&2 "usage: $program"
+    echo >&2 "usage: $program [-l salt-log-level ] [-r ssh-host ]"
     exit 2
 }
 logging=""
 
-while getopts "dl:?" o
+while getopts "dl:r:?" o
 do
     case "$o" in
-        d) set -x ;;                    # trace on
+        d) debug="-d"; set -x ;;                    # trace on
         l) logging="-l $OPTARG" ;;
+        r) remote="$OPTARG" ;;
         ?) fatal ;;                     # usage
     esac
 done
 shift $((OPTIND - 1))
 
 here=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -n "$remote" ]; then
+    dir=$(ssh "$remote" mktemp -d) || fatal "Could not create a remote tempdir"
+    echo $remote:$dir
+    (cd $here && tar -cf - $(find . -name .git -prune -o -type f) ) | ssh "$remote" "cd $dir && tar -xf - && sudo bash ./stand-alone.sh $debug $logging "
+    exit $?
+fi
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
